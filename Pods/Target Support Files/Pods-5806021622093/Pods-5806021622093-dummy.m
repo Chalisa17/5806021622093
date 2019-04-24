8cc0858e88bb0b4ba203c87dd12bb305d988a000
Admin <admin@Admins-iMac-21.local>
2019-04-24 15:26:44 +0700

    first commit

---
+++ Pods/Target Support Files/Pods-5806021622093/Pods-5806021622093-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_5806021622093 : NSObject
+@end
+@implementation PodsDummy_Pods_5806021622093
+@end
---
+++ Pods/Target Support Files/Pods-5806021622093UITests/Pods-5806021622093UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_5806021622093UITests : NSObject
+@end
+@implementation PodsDummy_Pods_5806021622093UITests
+@end
